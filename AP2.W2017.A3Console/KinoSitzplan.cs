d406fc49c81767111906fac883236e8c6834ccd7
wernerdc <manuel.werner3@gmx.de>
2025-09-14 21:39:58 +0200

    Projektdateien hinzufügen.

---
+++ AP2.W2017.A3Console/KinoSitzplan.cs
@@ -0,0 +1,63 @@
+﻿namespace AP2.W2017.A3Console
+{
+    public class KinoSitzplan
+    {
+        public bool[,] Seats { get; set; } = new bool[7, 30];
+        public KinoSitzplan()
+        {
+            Random rand = new();
+
+            for (int r = 0; r < Seats.GetLength(0); r++)
+            {
+                for (int i = 0; i < Seats.GetLength(1); i++)
+                {
+                    Seats[r, i] = rand.NextDouble() >= 0.5;
+                }
+            }
+        }
+
+        public void ToConsole(int seatNo, int count)
+        {
+            Console.WriteLine("Reihe | Sitz-Nummer");
+            Console.WriteLine("------|------------------------------------------------------------------------------------------------------------------------");
+
+            int seatRow = seatNo / 100 - 1;
+            int seatIndex = seatNo % 100 - 1;
+            
+            for (int r = 0; r < Seats.GetLength(0); r++)
+            {
+
+                ConsoleColor(1);
+                Console.Write($"  {r + 1}   | ");
+
+                for (int i = 0; i < Seats.GetLength(1); i++)
+                {
+                    if (r == seatRow && i >= seatIndex && i < seatIndex + count)
+                        Console.BackgroundColor = System.ConsoleColor.DarkGray;
+                    
+                    if (Seats[r, i])
+                        ConsoleColor(2);
+                    else 
+                        ConsoleColor(3);
+
+                    Console.Write($"{r + 1}{i + 1:D2} ");
+                    Console.BackgroundColor = System.ConsoleColor.Black;
+                }
+                Console.WriteLine();
+                ConsoleColor(1);
+            }
+        }
+
+        private static void ConsoleColor(int c)
+        {
+            Console.ForegroundColor = c switch
+            {
+                1 => System.ConsoleColor.Gray,
+                2 => System.ConsoleColor.Green,
+                3 => System.ConsoleColor.Red,
+                4 => System.ConsoleColor.Cyan,
+                _ => System.ConsoleColor.Gray,
+            };
+        }
+    }
+}
---
+++ AP2.W2017.A3Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+
+namespace AP2.W2017.A3Console
+{
+    internal class Program
+    {
+        static bool[,] kino = new bool[0,0];
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("AP2 I - Winter 2017 - Aufgabe 3 \n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            KinoSitzplan ks = new();
+            kino = ks.Seats;
+
+            int anzahl = 6;
+            int sitzNr = FreieSitze(anzahl);
+            
+            Console.WriteLine($"{anzahl} freie Sitze ab Platz: {sitzNr} \n");
+            ks.ToConsole(sitzNr, anzahl);
+
+            Console.WriteLine("\nPress [ENTER] to exit...");
+            Console.ReadLine();
+        }
+
+        public static int FreieSitze(int anzahlSitze)
+        {
+            int firstSeat = 0;
+
+            for (int r = 0; r < 7; r++)
+            {
+                int count = 0;
+                int row = (r + 1) * 100;
+
+                for (int i = 0; i < 30; i++)
+                {
+                    if (kino[r,i])
+                    {
+                        if (count == 0)
+                            firstSeat = row + i + 1;
+                    
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+
+                    if (count >= anzahlSitze)
+                        return firstSeat;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
